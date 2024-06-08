666b17770fa87df42d2bf3b81012b130bd7119a6
Айзат <Aimix444@gmail.com>
2024-06-08 14:06:07 +0300

    added episode controller

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus, HttpException, UseGuards } from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { CreateEpisodeDto } from './dto/create-episode.dto'
 import { UpdateEpisodeDto } from './dto/update-episode.dto';
 import { Episodes } from './episodes.model';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles-guard';
 
 @Controller('episodes')
 export class EpisodesController {
   constructor(private readonly episodesService: EpisodesService) {}
 
   @Post()
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
   async create(@Body() createEpisodeDto: CreateEpisodeDto): Promise<Episodes> {
     return this.episodesService.create(createEpisodeDto);
   }
@@ -28,6 +32,8 @@ export class EpisodesController {
   }
 
   @Patch(':id')
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
   async update(
     @Param('id') id: number,
     @Body() updateEpisodeDto: UpdateEpisodeDto,
@@ -40,6 +46,8 @@ export class EpisodesController {
   }
 
   @Delete(':id')
+  @Roles("ADMIN")
+  @UseGuards(RolesGuard)
   async remove(@Param('id') id: number): Promise<void> {
     await this.episodesService.remove(id);
   }
--- src/episodes/episodes.module.ts
+++ src/episodes/episodes.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { EpisodesController } from './episodes.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Episodes } from './episodes.model';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [ SequelizeModule.forFeature([Episodes]),],
+  imports: [SequelizeModule.forFeature([Episodes]), forwardRef(() => AuthModule)],
   providers: [EpisodesService],
   controllers: [EpisodesController],
   exports: [EpisodesService]
